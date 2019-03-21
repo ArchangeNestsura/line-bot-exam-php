2eac2839f9a0d554650ef4dd0e690cf0384d4a26
ArchangeNestsura <archange0001@gmail.com>
2019-03-21 21:45:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ieP2Up6VrHbnBzWwZ0jsDs/JRzfn/71h4zUvEjqWRViJoD6uoMs4fxXaXN7mCl3wSrlNJ0gLu86JivBLyNbxHHlYeAHXeXzxobtV6Yg5bzbVjkwiC4PtopzmXb4JST9Z5rnFTCaXZZNCmaNHaHDPBgdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
